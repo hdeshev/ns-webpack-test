487676e6698354afefde8998f888d2482b471d71
Hristo Deshev <hristo@deshev.com>
2016-08-20 01:40:42 +0300

    Move source code to src, bundle output goes to app.
    Add npm scripts to build/test android.

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: "my-app",
-    templateUrl: "app.component.html",
+    template: require("./app.component.html"),
 })
 export class AppComponent {
     public counter: number = 16;
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,45 @@
-var bundler = require("nativescript-dev-webpack");
+var webpack = require("webpack");
 //var ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
 var path = require("path");
 var failPlugin = require("webpack-fail-plugin");
 
-var config = bundler.getConfig({
-});
-
-config.context = path.resolve("./app");
-config.resolve.extensions.unshift(".ts");
-config.entry.bundle = "./main.ts";
-
-config.module = {
-    loaders: [
-        {
-            test: /\.ts$/,
-            loader: 'awesome-typescript-loader'
-        }
+module.exports = {
+    context: path.resolve("./src"),
+    entry: {
+        "bundle": "./main",
+        "tns-java-classes": "./tns-java-classes"
+    },
+    output: {
+        pathinfo: true,
+        path: path.resolve("./app"),
+        libraryTarget: "commonjs2",
+        filename: "[name].js",
+        jsonpFunction: "nativescriptJsonp"
+    },
+    resolve: {
+        extensions: [
+            ".ts",
+            "",
+            ".js",
+            ".android.js"
+        ],
+        modulesDirectories: [
+            "node_modules/tns-core-modules",
+            "node_modules"
+        ]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        failPlugin,
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ["tns-java-classes"]
+        }),
     ]
 };
-
-config.plugins.push(failPlugin);
-
-//config.plugins.push(new ForkCheckerPlugin());
-
-//console.log(JSON.stringify(config, null, "    "));
-module.exports = config;
